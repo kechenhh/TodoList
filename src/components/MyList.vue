fba18671be1334ffa754bfd3f76b046bea5c64b0
kechenhh <923407526@qq.com>
2021-09-04 14:01:57 +0800

    初始添加

---
+++ src/components/MyList.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="todo-main">
+    <my-item
+      v-for="todoOgj in todos"
+      :key="todoOgj.id"
+      :todo="todoOgj"
+    ></my-item>
+  </ul>
+</template>
+
+<script>
+import MyItem from "./MyItem";
+
+export default {
+  name: "MyList",
+  components: {
+    MyItem
+  },
+  props:['todos'],
+  data() {
+    return {
+
+    };
+  },
+
+};
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
